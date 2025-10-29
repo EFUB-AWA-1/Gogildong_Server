c95fb114437740af72a79a6b560024e6a0434188
yepot <yes03326@gmail.com>
2025-10-29 18:27:09 +0900

    chore: 초기 환경 세팅

---
+++ gogildong/src/main/java/com/efub/gogildong/GogildongApplication.java
@@ -0,0 +1,13 @@
+package com.efub.gogildong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogildongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogildongApplication.class, args);
+	}
+
+}
---
+++ gogildong/src/test/java/com/efub/gogildong/GogildongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.gogildong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogildongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
