8d70666ede7aef2433432ab3ab6f33c033fb3e07
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-11-05 17:09:06 +0900

    feat(#19): 유저 유효성 검사 추가

--- gogildong/src/main/java/com/efub/gogildong/schools/service/SchoolViewRequestService.java
+++ gogildong/src/main/java/com/efub/gogildong/schools/service/SchoolViewRequestService.java
@@ -2,6 +2,7 @@ package com.efub.gogildong.schools.service;
 
 import com.efub.gogildong.global.exception.ExceptionCode;
 import com.efub.gogildong.global.exception.GoGildongException;
+import com.efub.gogildong.schools.domain.RequestStatus;
 import com.efub.gogildong.schools.domain.School;
 import com.efub.gogildong.schools.domain.SchoolViewRequest;
 import com.efub.gogildong.schools.dto.request.SchoolViewRequestRequest;
@@ -27,5 +28,16 @@ public class SchoolViewRequestService {
         SchoolViewRequest viewRequest = request.toEntity(school, user);
         schoolViewRequestRepository.save(viewRequest);
         return SchoolViewRequestResponse.from(viewRequest);
+    }
+
+    // 해당 학교, 사용자에게 열람 권한이 있는지 확인합니다.
+    public void validateViewRequestBySchoolAndUser(School school, User user) {
+        SchoolViewRequest schoolViewRequest = schoolViewRequestRepository.findBySchoolAndUser(school, user)
+                .orElseThrow(() -> new GoGildongException(ExceptionCode.UNAUTHORIZED_SCHOOL_ACCESS));
+
+        // 해당 학교 소속 아니면서, 열람 요청 받아서 승인 받지 않은 경우
+        if(!(user.getSchool().equals(school) || schoolViewRequest.getStatus().equals(RequestStatus.APPROVED))) {
+            throw new GoGildongException(ExceptionCode.UNAUTHORIZED_SCHOOL_ACCESS);
+        }
     }
 }
