c9878fe2d68aafc67d0bc99b1fd62a3fd6f3d5e2
yepot <yes03326@gmail.com>
2025-11-07 21:28:17 +0900

    feat(#52): 제보 전체 목록 조회

--- gogildong/src/main/java/com/efub/gogildong/reports/repository/ReportRepository.java
+++ gogildong/src/main/java/com/efub/gogildong/reports/repository/ReportRepository.java
@@ -5,8 +5,13 @@ import com.efub.gogildong.reports.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    // 최신순으로 모든 제보 불러오기
+    List<Report> findByOrderByCreatedAtDesc();
     List<Report> findAllByFacility(Facility facility);
 }
