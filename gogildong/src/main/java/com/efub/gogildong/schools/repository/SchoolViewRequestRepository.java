95e6ae86aec611d6b9614e2d9ce6cc79422526ba
yepot <yes03326@gmail.com>
2025-11-18 01:28:07 +0900

    feat(#60): 학교 열람 요청 목록 조회 api

--- gogildong/src/main/java/com/efub/gogildong/schools/repository/SchoolViewRequestRepository.java
+++ gogildong/src/main/java/com/efub/gogildong/schools/repository/SchoolViewRequestRepository.java
@@ -6,10 +6,12 @@ import com.efub.gogildong.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SchoolViewRequestRepository extends JpaRepository<SchoolViewRequest, Long> {
     Optional<SchoolViewRequest> findBySchoolAndUser(School school, User user);
     Optional<SchoolViewRequest> findByRequestId(Long requestId);
+    List<SchoolViewRequest> findAllByOrderByRequestedAtDesc();
 }
