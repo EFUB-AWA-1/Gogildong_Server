aba05e04ea4cc3b9b247fa29d2b8f2cfcc1d97e3
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-11-25 18:05:06 +0900

    feat(#85): 엽전 증감 api

---
+++ gogildong/src/main/java/com/efub/gogildong/shops/service/CoinService.java
@@ -0,0 +1,29 @@
+package com.efub.gogildong.shops.service;
+
+import com.efub.gogildong.global.exception.ExceptionCode;
+import com.efub.gogildong.global.exception.GoGildongException;
+import com.efub.gogildong.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CoinService {
+
+    @Transactional
+    public void earnCoin(User user, int amount) {
+        int before = user.getCoin();
+        user.setCoin(before + amount);
+    }
+
+    @Transactional
+    public void spendCoin(User user, int amount) {
+        int before = user.getCoin();
+        int after = user.getCoin() - amount;
+        if(after < 0) {
+            throw new GoGildongException(ExceptionCode.COIN_INSUFFICIENT_BALANCE);
+        }
+        user.setCoin(before - amount);
+    }
+}
