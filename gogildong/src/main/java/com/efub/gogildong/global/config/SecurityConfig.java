9ba6a4edf70ca7818564ff2b45e8923e2e2595a6
gimye <gimyedev@gmail.com>
2025-12-06 04:06:04 +0900

    chore: cors 추가

--- gogildong/src/main/java/com/efub/gogildong/global/config/SecurityConfig.java
+++ gogildong/src/main/java/com/efub/gogildong/global/config/SecurityConfig.java
@@ -75,7 +75,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",       // 로컬 개발용
                 "https://localhost:5173",
-                "https://gogildong.vercel.app"     // 배포 프론트
+                "https://gogildong.vercel.app",     // 배포 프론트
+                "https://192.168.35.42:5173"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
