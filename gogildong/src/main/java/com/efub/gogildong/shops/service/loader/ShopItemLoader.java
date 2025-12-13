e4e41e40106b773c520eafe31cbd9563d5fb3387
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-12-13 17:29:53 +0900

    feat(#141): 모자 이미지 입력

--- gogildong/src/main/java/com/efub/gogildong/shops/service/loader/ShopItemLoader.java
+++ gogildong/src/main/java/com/efub/gogildong/shops/service/loader/ShopItemLoader.java
@@ -25,14 +25,14 @@ public class ShopItemLoader implements ApplicationRunner {
 
 
         // --- 일반 상점 아이템 ---
-        insertIfNotExists("검정 머리", "/head/black.svg", "/head/black.svg", ClotheType.HEAD, 30, false);
-        insertIfNotExists("갈색 머리", "/head/brown.svg", "/head/brown.svg", ClotheType.HEAD, 35, false);
-        insertIfNotExists("캡모자", "/head/cap.svg", "/head/cap.svg", ClotheType.HEAD, 30, false);
-        insertIfNotExists("삐에로 모자", "/head/clown.svg", "/head/clown.svg", ClotheType.HEAD, 40, false);
-        insertIfNotExists("판다 모자", "/head/panda.svg", "/head/panda.svg", ClotheType.HEAD, 50, false);
-        insertIfNotExists("분홍 머리", "/head/pink.svg", "/head/pink.svg", ClotheType.HEAD, 35, false);
-        insertIfNotExists("상투", "/head/sangtoo.svg", "/head/sangtoo.svg", ClotheType.HEAD, 20, false);
-        insertIfNotExists("산타 모자", "/head/santa.svg", "/head/santa.svg", ClotheType.HEAD, 42, false);
+        insertIfNotExists("검정 머리", "/head/black.svg", "/head/black-hat.svg", ClotheType.HEAD, 30, false);
+        insertIfNotExists("갈색 머리", "/head/brown.svg", "/head/brown-hat.svg", ClotheType.HEAD, 35, false);
+        insertIfNotExists("캡모자", "/head/cap.svg", "/head/cap-hat.svg", ClotheType.HEAD, 30, false);
+        insertIfNotExists("삐에로 모자", "/head/clown.svg", "/head/clown-hat.svg", ClotheType.HEAD, 40, false);
+        insertIfNotExists("판다 모자", "/head/panda.svg", "/head/panda-hat.svg", ClotheType.HEAD, 50, false);
+        insertIfNotExists("분홍 머리", "/head/pink.svg", "/head/pink-hat.svg", ClotheType.HEAD, 35, false);
+        insertIfNotExists("상투", "/head/sangtoo.svg", "/head/sangtoo-hat.svg", ClotheType.HEAD, 20, false);
+        insertIfNotExists("산타 모자", "/head/santa.svg", "/head/santa-hat.svg", ClotheType.HEAD, 42, false);
 
         insertIfNotExists("삐에로 옷", "/body/clown-cloth.svg", "/body/clown-cloth.svg", ClotheType.DRESS, 42, false);
         insertIfNotExists("곤룡포", "/body/king.svg", "/body/king.svg", ClotheType.DRESS, 100, false);
