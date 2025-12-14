717cd31ba6eb733b5bdd3263b1d52252e8817364
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-12-15 00:35:39 +0900

    fix: 태그 카테고리 업데이트

--- gogildong/src/main/java/com/efub/gogildong/schools/domain/constants/TagCategory.java
+++ gogildong/src/main/java/com/efub/gogildong/schools/domain/constants/TagCategory.java
@@ -3,7 +3,7 @@ package com.efub.gogildong.schools.domain.constants;
 import com.efub.gogildong.schools.domain.TagName;
 
 public enum TagCategory {
-    all, restroom, elevator, ramp;
+    all, restroom, elevator, classroom;
 
     /*
     * param 값인 tagCategory를 데이터베이스에 저장된 tag 이름으로 변경합니다.
@@ -12,7 +12,7 @@ public enum TagCategory {
         return switch (this) {
             case restroom -> TagName.장애인_화장실;
             case elevator -> TagName.엘리베이터;
-            case ramp -> TagName.경사로;
+            case classroom -> TagName.교실;
             case all -> null;
         };
     }
